71fdbd48d8a0be4a72854a500439860fa312ea8d
charlesliudev <charlesliu205@gmail.com>
2020-09-06 13:03:21 -0700

    changed color scheme

---
+++ flask-server/static/react/precache-manifest.29b55b5d1a123a9c984b498670fbc467.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7abdd448f87e40689dad93e082ac5a93",
+    "url": "/static/react/../../templates/index.html"
+  },
+  {
+    "revision": "0b64c371c8033aadd751",
+    "url": "/static/react/js/2.4ec4d235.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/react/js/2.4ec4d235.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81855854dd36e530ea30",
+    "url": "/static/react/js/main.21b58495.chunk.js"
+  },
+  {
+    "revision": "00b90c6979e7b9785281",
+    "url": "/static/react/js/runtime-main.3a44c33a.js"
+  }
+]);
